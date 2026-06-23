8cf6834890382f98b5cda7bcb118f4b9742f1102
Pistacker <Thepewst@gmail.com>
2026-06-24 01:37:13 +0200

    restart from htmlcss

--- api/utils.js
+++
@@ -1,7 +0,0 @@
-export function createErrorResponse(message, status = 400) {
-  return { statusCode: status, body: JSON.stringify({ error: message }) };
-}
-
-export function createSuccessResponse(payload, status = 200) {
-  return { statusCode: status, body: JSON.stringify(payload) };
-}
